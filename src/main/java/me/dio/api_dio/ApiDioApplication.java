31972de0ed2cae3e7ae109e7661b4a0c8f08049b
LeCaVeiRa <39318329+LeCaVeiRa@users.noreply.github.com>
2025-03-07 15:28:20 -0300

    fisrt commit

---
+++ src/main/java/me/dio/api_dio/ApiDioApplication.java
@@ -0,0 +1,13 @@
+package me.dio.api_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/api_dio/ApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.api_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
